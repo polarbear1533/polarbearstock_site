6988dc2dd7088eb99b2bfe4d6eb13304c7847214
hh992384 <61733053+hh992384@users.noreply.github.com>
2020-08-08 19:47:25 -0700

    update

---
+++ _site/assets/js/lunrsearchengine.js
@@ -0,0 +1,129 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/404.html",
+    "title": "404",
+    "body": "404 Page not found!Please use the search bar from the bottom left or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/about",
+    "title": "Memoirs, a free minimalist Jekyll blogging theme with modern design",
+    "body": "This website is a demonstration to see Memoirs Jekyll theme in action. The theme is compatible with Github pages, in fact even this demo itself is created with Github Pages and hosted with Github.  Get Memoirs for Jekyll → "
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/contact",
+    "title": "加入我们",
+    "body": " 微信群:  微博:  老虎证劵: 刚刚注册, 欢迎国内炒美股的股民关注捧场!  我的邀请码是 LL45RQ, 点这里 跳转到邀请海报, 可以扫描二维码开户.  如果以上信息过期, 请联系管理员进行更新: 微信ID: cc18723aa "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/",
+    "title": "Home",
+    "body": "                                                                                               我们提供的服务                         1 2 3 4 5                      :       短线以及超短线看涨目标, 每周K线分析目标列表, 大盘走势分析, 美股知识解说以及技巧分享. 请点击标题查看详情. :                                                                               polarbear                                                                                                                                                           成果分享! [2020年8月7日更新]                         1 2 3 4 5                      :       晒成果~ 😀 如果你是新人, 强烈建议先阅读一遍我总结的美股短线入门必读. :                                                                               polarbear                                                                                                                                                           大盘每日点评 [2020年8月6日更新]                         1 2 3 4 5                      :       每日更新的大盘点评, 及时总结新闻消息. :                                                                               polarbear                                                                                                                                                           美股短线入门必读                         1 2 3 4 5                      :       对于美股新手需要注意的规则, 以及分出比较常见的短线看涨原因, 比较风险等级. :                                                                               polarbear                                                                                                                                                           美股基础知识: 财务报告                         1 2 3 4 5                      :       如题, 以简单易懂的语言介绍美股的财务报告. :                                                                               polarbear                                                                                                                                                           美股基础知识: 动力/冲势                         1 2 3 4 5                      :       动力or Momentum 是一个判断合适卖出时机的一个非常非常重要的特性, 另外也可以避免高位买入. :                                                                               polarbear                                                                 "
+    }, {
+    "id": 5,
+    "url": "http://localhost:4000/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 6,
+    "url": "http://localhost:4000/pinned-page-1/",
+    "title": "我们提供的服务",
+    "body": "2028/01/30 -  短线看涨目标 超短线看涨目标 每周K线分析目标 (建设中…) 大盘走势分析 提供新闻资讯 (建设中…) 美股知识解说以及技巧分享 利用iPhone的“紧急来电例外规则”定制美股闹钟 (建设中…)下面我会逐一解释内容 短线和超短线最大的不同在与持有时间以及风险, 短线通常预计持有几天到2个星期, 而超短线是指在同一天卖出, 或者持过一个晚上, 或者周末, 每单必带止损价, 因为风险挺大的, 当然如果抓住了盈利也是非常可观的, 我给常见的看涨理由分了类: 1. 庄家洗盘 2. 暴跌后的反弹  3. 追Rumor/News (赌未来将要发布的新闻或者消息)4. 追R/S (反向拆股)5. 追Bankruptcy(濒临破产的股票)6. 追暴涨股 关于更多细节, 何时卖, 几个重要参考因素, 我写在了另一边帖子里 美股短线入门必读, 是我个人总结的一些经验, 适合今年动荡不安的市场状况. 另外记住这一点, 涨了就卖, 错过了买入价请放手, Let it go, 机会一直都会有, 不要等到最高点再卖.  如果没有其他重要的新闻催化剂, 变化幅度最大的时候大部分会第一天发生, 而不是第二天, 第三天 每周K线分析目标传统的WatchList, 每周更新的列表包含数支股票, 提供突破价和止盈价, 没有到达突破价之前不会买入, 借势盈利. 大盘走势分析, 我会尽量做到每日更新, 及时总结新闻消息, 提醒大家熊市/牛市的转换. 提供新闻资讯, 这个也需要大家的帮忙, 不仅仅是对于目标的股票, 也包括各种和股市相关的新闻. 美股知识解说以及技巧分享, 以简单易懂的语言整理美股的经验, 我会持续更新这个列表.  美股基础知识: 财务报告 美股基础知识: 动力/冲势利用iPhone的“紧急来电例外规则”定制美股超短线闹钟, [原理解释], 实现实时的目标更新通知!  设好铃声“雷达”, 从此安心上班睡觉, 再也不用担心错过盘前暴涨以及最新消息. 欢迎新人 加入 👋, 目前还在逐步完善中, 感谢大家帮忙推广, 为我提供更多动力建设更多功能. "
+    }, {
+    "id": 7,
+    "url": "http://localhost:4000/pinned-page-2/",
+    "title": "成果分享! [2020年8月7日更新]",
+    "body": "2028/01/29 - 有兴趣的可以 加入 微信群, 未来的提醒以及目标更新也会发在微信群里, 如果你是新人, 强烈建议先阅读一遍我总结的 美股短线入门必读. 废话不多说了, 晒图 8月7日, 2020 $ACER, 短线, 8月7日同一天, 盈利+19%!这支涨的比预计的提前了, 群里不少人也抓住了机会 👏👏, 我之前已经提到过了, 目标价只是参考作用, 涨了就卖, 美股这么多公司, 机会一直都有.  8月6日, 2020今天开始短线改用老虎证劵 😃, 为什么用老虎证劵?充了5000刀玩玩. 老虎证劵第一笔交易 🤑 开门红  $KZIA, 超短线, 8月6日收盘前15分钟, 8. 5-9. 35, 盈利+10%! $KZIA, 超短线, 8月6日收盘前跌回8块, 再次买入, 隔天盘前高位止盈, 盈利+20%! 8月5日, 2020 $IMBI, 超短线, 8月5日同一天, 盈利+10%! 8月3日, 2020 $FTNT期权, 短线, 8月3日-8月5日, 盈利+10%! $WISA, 短线, 7月23日-8月5日, 盈利+12%!  7月31日, 2020 $HIHO, 超短线, 7月31日收盘前买入, 持过周末, 8月3日周一开盘前高抛, 盈利+20%! 7月29日, 2020 $SBPH, 超短线, 7月29日同一天, 盈利+6%! 结果隔天早上暴涨40%!😂  7月26日, 2020 $WINT, 短线, 7月27日-8月3日, 盈利目标+30%达成! 7月23日, 2020 $SPCE, 短线, 7月23日-7月28日, 盈利+5-10% 7月22日, 2020 $NSPR, 短线, 7月22日-7月23日, 盈利+17% 7月21日, 2020 $TWTR期权, 短线, 7月21日-7月23日, 盈利+22% 7月15日, 2020 $BOXL, 短线, 7月15日-7月16日, 盈利+112. 63% $ICD, 短线, 7月15日-7月22日, 盈利+34. 57% "
+    }, {
+    "id": 8,
+    "url": "http://localhost:4000/pinned-page-3/",
+    "title": "大盘每日点评 [2020年8月6日更新]",
+    "body": "2028/01/28 - 我会尽量做到每日更新, 及时总结新闻消息, 提醒大家熊市/牛市的转换. 8月7日, 2020当天中午抛售使纳斯达克下跌超过1. 24％，但随后在晚些时候回升，但收盘时依然下跌了不到1％.  道琼斯工业平均指数和标准普尔指数以绿色收盘, 两个指数连续六个交易日上涨, 市场仍在等待发放援助金(Stimulus Check)的消息，虽然这是一个很强大的催化剂, 但请保持理智, 不要过于乐观, 任何一点失望的结果都可能会导致市场开始抛售. 7月23日, 2020大部分时候我炒的股票因为市值很小所有和大盘关联不大, 所以以前并不是很常更新大盘点评, 以后我会尽量做到每日更新. 这是之前的.  "
+    }, {
+    "id": 9,
+    "url": "http://localhost:4000/pinned-page-4/",
+    "title": "美股短线入门必读",
+    "body": "2028/01/27 - 首先, 不要把新闻报道说 “因为XXX, 大盘暴涨” 真当回事, 大盘在连续跌了几天后, 极小的利好消息都会导致大盘暴涨. 另一个情况同样适用, 大盘在连续涨了几天后, 极小的坏消息也会导致大盘暴跌.  涨和跌其实一直是轮流, 新闻只是起到”导火线”和”催化剂”的作用, 总体来说就是一个循环割韭菜的操作. 这点相信大部分人都清楚, 但是在真正操作的时候还是会做出高买低卖, 低卖高买(做空)的事情, 也是因为这样, 你需要给自己定一些规则严格执行. 无论涨和跌, 都有几个共同点:  如果没有其他重要的新闻催化剂, 变化幅度最大的时候大部分会第一天发生, 而不是第二天, 第三天…     暴涨的第一天, 各种散户疯狂买入, 这种价格一般下午就要开始回落, 后面几天都很难再涨回去了.   » 有两个时间段最适合在暴涨的时候清仓或者至少卖掉一部分用于洗盘(注释1), 请看下面的(注解2)   暴跌的第一天, 散户绝望割肉, 其实如果一直拿着, 等到当天快结束了, 可以减少很多损失.    另外周五(以及周四)下跌可能性要比一周内任何一天都要大很多, 如果周一到周四都在涨, 周五很大概率就会回拉一波, 很常见也是很健康的现象. 请阅读我每日更新的 大盘点评 以注意局势的变化, 保持”不要过度悲观或者乐观”的心理. (注释1): 洗盘所指内容是, 即使你很看好这个股票, 暴涨的时候也可以考虑清仓或者卖掉一部分, 晚些时候跌回来了再买回去, 做的好同一支股票短时间内可以盈利很多, 不过需要大量练习才能克服心理障碍. 注意最好预先设好止盈价, 可以是6%或者10%, 随你自己定不要太贪, 这个尤其重要当炒超短线的时候, 涨到10%先卖了再说. (注释2):    第一个时间段: 盘前8:30 至 开盘前, 这个是 Robinhood用户进场的时候, 你可以理解成最大的散户群体, 也就是被割韭菜的主要对象, 这段时间内如果价格冲的很高建议清仓或者卖掉一部分, 因为大概率开盘后10分钟内会跌回来, 甚至有可能将盘前的涨势全部抹平, 出现跌势.     第二个时间段: 开盘后 1个小时至2个小时左右 的时间段, 这个时候通常是一天市场 Volume(交易量/活跃度)最大的时候, 牛市会出现Overbought(溢价)很适合套现, 熊市会出现 Oversold(折价)适合买入等待反弹时机.  我上面所提到的时间都是美国东部时间, 即纽约时间. 举一个某天指数下跌市场开始抛售的例子，比如 $SAVE – 变化幅度最大的航空股, 开盘后刚刚好一小时就是底部, 这样一波反弹操作抓住了就是+10%的盈利: 美股短线以及超短线看涨主要是这几种情况: 1. 庄家洗盘无理由涨和跌, 幅度通常都不大, 5%-15%.  如果过去这个行为已经重复了数次了, 且低价接近 52w low, 妥妥的低风险短线.  止损也很好找, 如果跌破 52周最低 就卖, 可以把亏损控制得很小. 2. 暴跌后的反弹高风险, 属于超短线的, 同一天或者隔天卖出, 大部分暴跌都会有大幅度反弹, 但是主要风险在于抄底抄的万一不是底, 那就成接刀子的了… 什么时候适合接, 请看上面的 $SAVE 例子. 3. 追Rumor/News (赌未来将要发布的新闻或者消息)这个是时间最长的短线, 因为持有时间长所以每次的风险其实都是不确定的, 不知道哪天会drop一个坏消息或者好消息. .  但同时这个也是最有潜力的类型, 突然翻倍或者暴涨50%以上大部分情况都是有着大新闻作为背景的.  真正其中赚到钱的人可能已经持有几个月了, 他们经过认真分析觉得有理由相信将来会有一个利好新闻, 加上耐心. 4. 追R/S (反向拆股)Reverse Split, 即减少股票份额, 将股价抬高, 其实本身价格没有变化.   很多股票宣布即将到来的反向拆股之前会有人追, 小股票宣布反向拆股很多时候会同时宣布和另一家公司合并, 另一家公司可能原本并不是上市公司, 因此也想借此上市, 反向拆股完成后, 将会生成新的股票代码, 旧股份将会自动转化为新股, 有些时候公司也会给予额外股份奖励, 算是一种特别的新闻.  公司有时候会表示一个目标价, 拆完股后计划要将股价抬到这么高. 而一个公司一般需要这么做的时候, 股价已经很低很难看了, 进行R/S其实是很不好的消息.  反向拆股过后通常是接着是一顿暴跌, 因为公司本身没有义务短时间内要将股价抬到所宣布的目标价. 5. 追Bankruptcy(濒临破产的股票)这个情况比较特殊, 风险极大. 一只股票遭受利空新闻后, 比如宣布破产准备, CEO因为诈骗被抓, 等等破坏性极大的消息, 股价严重下跌, 但是连续下跌一段时间后, 大概率至少会出现一次暴涨 😂, 股价飙升数倍, 甚至破产的前一天都有可能发生这个情况. 造成这个现象我认为可能的原因是这个  大盘在连续跌了几天后, 极小的利好消息都会导致大盘暴涨. 另一个情况同样适用, 大盘在连续涨了几天后, 极小的坏消息也会导致大盘暴跌. 追这样股票的绝大部分都是散户, 从一个网站 https://robintrack. net, 看到股价和散户买入人数的变化曲线. 今年例子挺多的: $AMC, $JCP, $WCAGY…  $HTZ: 从5块跌到1块以下, 然后数天内又飙回5块, 真刺激😂. 6. 追暴涨股:因为暴涨的股票会被很多平台列到主界面, 随着曝光度的提高, 交易量也会猛增, 这个是普遍的超短线本质, 大量散户之间的博弈, 讲究下手快, 然后设一个止盈点, 在6-20%范围内的, 涨到了后自动卖出, 另外为了保险起见, 止损价一定一定一定要在成本价之上, 具体例子可以参考 这里 .    盘后追, 赌明早盘前会继续涨. 要注意两个可能导致交易量暴涨的时间点, 美东时间7:00AM (TD Ameritrade用户所允许最早的盘前交易时间), 以及美东时间9:00AM (Robinhood用户所允许最早的盘前交易时间).   主要是后者, 安全的做法是在美东时间8:30AM之前买进去, 看看股价对于大量散户的加入, 会出现怎样的变化, 如果在开盘前, 股价被刺激到了涨了, 卖, 如果随着交易量暴涨股价都没有动力继续涨了, 这种情况止损一定要严格执行.     盘前也可以追, 不过要记得早点入场, 设好止盈价, 卖卖卖, 感觉有点像是加快了数倍的期权交易, 开盘就是到期日. 刺激. .  关于短线以及超短线, 我还是想提醒下: 想要短线炒股赚钱有两个致命行为需要改正:  涨了不卖, 一支股票如果暴涨了(大于9%), 尤其是盘前的时候, 涨的第一天至少应该卖掉一部分! 这个是短线的真正意义所在, 涨了就卖! 目标价只是起到一个参考作用, 也可能只涨了一半, 也可能直接翻倍, 请先把当下的盈利赚到手, 不要担心错过机会, 机会以后一直都会有的.  其他股票卖了钱, 拿着不自在, 就是很想找个股票买进去. 这个是赌徒心理, 怎么改? 赚了钱取出来, 永远放固定的钱在股市里. 另外我发布通知后已经涨过的短线目标不要再入了. 等待下一个机会, 要有耐心, 心急吃不了热豆腐. "
+    }, {
+    "id": 10,
+    "url": "http://localhost:4000/pinned-page-5/",
+    "title": "美股基础知识: 财务报告",
+    "body": "2028/01/26 -  "
+    }, {
+    "id": 11,
+    "url": "http://localhost:4000/pinned-page-6/",
+    "title": "美股基础知识: 动力/冲势",
+    "body": "2028/01/25 -   "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><span class='body'>"+ body +"</span><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><small><span class='body'>"+ body +"</span><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
